02c5a27046913882f5839cc52a2a3539483ea877
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-23 12:26:44 -0500

    Demo json imports
    Demo json imports

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'example-json-import/config/environment';
 
+import data from './sample.json';
+
+console.log({ data });
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    autoImport: {
+      allowAppImports: ['**/*.json'],
+    },
     // Add options here
   });
 
